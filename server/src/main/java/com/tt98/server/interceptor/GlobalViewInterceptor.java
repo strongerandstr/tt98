f4f57d12f9c0bcec2bb8d18eecf6bae23cc23256
strongerandstr <Y987359269@gmail.com>
2024-05-06 21:48:45 +0800

    5.6 尝试搞首页

---
+++ server/src/main/java/com/tt98/server/interceptor/GlobalViewInterceptor.java
@@ -0,0 +1,26 @@
+package com.tt98.server.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.servlet.AsyncHandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class GlobalViewInterceptor implements AsyncHandlerInterceptor {
+    @Autowired
+    private GlobalInitService globalInitService;
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView){
+        if(!ObjectUtils.isEmpty(modelAndView)){
+            if(response.getStatus() == HttpStatus.OK.value()){
+                modelAndView.getModel().put("global", globalInitService.globalAttr());
+            }
+        }
+    }
+
+}
